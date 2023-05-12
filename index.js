6d300920f9917a76bf3d3a48780073205b800e2e
laumonnier <laurent.p.monnier86@gmail.com>
2023-05-12 10:26:05 +0200

    Background animation of the home section

--- index.js
+++ index.js
@@ -3,17 +3,17 @@
 const scroll = new LocomotiveScroll({
     el: document.querySelector('[data-scroll-container]'),
     smooth: true,
-    // tablet: {smooth: true},
-    // smartphone: {smooth: true},
+    tablet: {smooth: true},
+    smartphone: {smooth: true},
 });
 
-// //Body color
-// scroll.on("scroll", () => {
-//     if (document.querySelector(".about-section__title.is-inview")) {
-//         document.querySelector(".about-section").style.background = "#000";
-//         document.querySelector(".about-section").style.color = "#FFF";
-//     }else{        
-//         document.querySelector(".about-section").style.background = "#FFF";
-//         document.querySelector(".about-section").style.color = "#000";
-//     }
-// });
+//Body color
+scroll.on("scroll", () => {
+    if (document.querySelector(".about-section__title.is-inview")) {
+        document.querySelector(".about-section").style.background = "#000";
+        document.querySelector(".about-section").style.color = "#FFF";
+    }else{        
+        document.querySelector(".about-section").style.background = "#FFF";
+        document.querySelector(".about-section").style.color = "#000";
+    }
+});
