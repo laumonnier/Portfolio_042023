f8319a9862e45596de90ca2fb6185f4b522bafb0
laumonnier <laurent.p.monnier86@gmail.com>
2023-05-12 21:31:10 +0200

    Added a different font Bungee+Inline with a graphic effect.

--- index.js
+++ index.js
@@ -17,3 +17,45 @@ scroll.on("scroll", () => {
         document.querySelector(".about-section").style.color = "#000";
     }
 });
+
+var message = "Hello World!!! I'm Laurent MONNIER";
+var msgCount = 0;
+var blinkCount = 0;
+var blinkFlg = 0;
+var timer1, timer2;
+var messageName = document.getElementById("messageName");
+
+function textFunc() {
+    messageName.innerHTML = message.substring(0, msgCount);
+
+        if(msgCount == message.length) {
+            //Stop Timer
+            clearInterval(timer1);
+
+            //Start blinking animation!
+            // timer2 = setInterval("blinkFunct()", 200);
+        }else {
+            msgCount++;
+        }
+    
+}
+
+function blinkFunct() {
+
+    // Blink 10 times
+    if (blinkCount < 11) {
+        if(blinkFlg == 0) {
+            messageName.innerHTML = message;
+            blinkFlg = 1;
+            blinkCount++;
+        } else {
+            messageName.innerHTML = "";
+            blinkFlg = 0;
+        }
+    } else {
+        //Stop Timer
+        clearInterval(timer2);
+    }
+}
+
+timer1 = setInterval("textFunc()", 150); //Every 150 milliseconds
